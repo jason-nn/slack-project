b3a21f44a32ba9038abefdfd1f526ecd08d6ed18
Jason Ho <jason.ho@obf.ateneo.edu>
2024-05-31 23:21:49 +0800

    Enable launch on local

--- src/App.js
+++ src/App.js
@@ -1,108 +1,108 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import Toast from "./Components/Shared/Toast";
-import Loading from "./Components/Shared/Loading";
-import Home from "./Components/Shared/Home";
-import Login from "./Components/Shared/Login";
-import { Config } from "./Utilities/Config";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Toast from './Components/Shared/Toast';
+import Loading from './Components/Shared/Loading';
+import Home from './Components/Shared/Home';
+import Login from './Components/Shared/Login';
+import { Config } from './Utilities/Config';
 
 export default function App() {
-    // axios.defaults.baseURL = "http://206.189.91.54/api/v1/";
-    axios.defaults.baseURL = "https://slackapi.avionschool.com/api/v1";
+  // axios.defaults.baseURL = "http://206.189.91.54/api/v1/";
+  axios.defaults.baseURL = 'https://slack-avion.netlify.app/api/v1';
 
-    const [UserData, setUserData] = useState(null);
-    const [UserHeaders, setUserHeaders] = useState(null);
-    const [DisplayLoading, setDisplayLoading] = useState(true);
+  const [UserData, setUserData] = useState(null);
+  const [UserHeaders, setUserHeaders] = useState(null);
+  const [DisplayLoading, setDisplayLoading] = useState(true);
 
-    const [Message, setMessage] = useState(null);
-    const [Success, setSuccess] = useState(null);
-    const [Error, setError] = useState(null);
+  const [Message, setMessage] = useState(null);
+  const [Success, setSuccess] = useState(null);
+  const [Error, setError] = useState(null);
 
-    setTimeout(() => {
-        setDisplayLoading(false);
-    }, 3300);
+  setTimeout(() => {
+    setDisplayLoading(false);
+  }, 3300);
 
-    useEffect(() => {
-        if (localStorage.email && localStorage.password) {
-            const email = localStorage.email;
-            const password = localStorage.password;
+  useEffect(() => {
+    if (localStorage.email && localStorage.password) {
+      const email = localStorage.email;
+      const password = localStorage.password;
 
-            const data = {
-                email,
-                password,
-            };
+      const data = {
+        email,
+        password,
+      };
 
-            axios(Config.SignIn(data))
-                .then((response) => {
-                    setUserData(response?.data?.data);
-                    setUserHeaders(response?.headers);
-                    setError(null);
-                    setMessage(null);
-                    setSuccess("Logging in");
-                    setTimeout(() => {
-                        setSuccess(null);
-                    }, 2000);
-                })
-                .catch((error) => {
-                    console.log(error);
-                    setMessage(null);
-                    setSuccess(null);
-                    setError("Error");
-                    setTimeout(() => {
-                        setError(null);
-                    }, 2000);
-                });
-        }
-    }, []);
+      axios(Config.SignIn(data))
+        .then((response) => {
+          setUserData(response?.data?.data);
+          setUserHeaders(response?.headers);
+          setError(null);
+          setMessage(null);
+          setSuccess('Logging in');
+          setTimeout(() => {
+            setSuccess(null);
+          }, 2000);
+        })
+        .catch((error) => {
+          console.log(error);
+          setMessage(null);
+          setSuccess(null);
+          setError('Error');
+          setTimeout(() => {
+            setError(null);
+          }, 2000);
+        });
+    }
+  }, []);
 
-    return (
+  return (
+    <>
+      {Message ? (
         <>
-            {Message ? (
-                <>
-                    <Toast className="Message Toast" text={Message} />
-                </>
-            ) : null}
-            {Success ? (
-                <>
-                    <Toast className="Success Toast" text={Success} />
-                </>
-            ) : null}
-            {Error ? (
-                <>
-                    <Toast className="Error Toast" text={Error} />
-                </>
-            ) : null}
-            {DisplayLoading ? (
-                <>
-                    <Loading />
-                </>
-            ) : null}
-
-            {UserData && UserHeaders ? (
-                <>
-                    <Home
-                        UserData={UserData}
-                        UserHeaders={UserHeaders}
-                        setUserData={(i) => setUserData(i)}
-                        setUserHeaders={(i) => setUserHeaders(i)}
-                        setDisplayLoading={(i) => setDisplayLoading(i)}
-                        setMessage={(i) => setMessage(i)}
-                        setSuccess={(i) => setSuccess(i)}
-                        setError={(i) => setError(i)}
-                    />
-                </>
-            ) : (
-                <>
-                    <Login
-                        setUserData={(i) => setUserData(i)}
-                        setUserHeaders={(i) => setUserHeaders(i)}
-                        setDisplayLoading={(i) => setDisplayLoading(i)}
-                        setMessage={(i) => setMessage(i)}
-                        setSuccess={(i) => setSuccess(i)}
-                        setError={(i) => setError(i)}
-                    />
-                </>
-            )}
+          <Toast className="Message Toast" text={Message} />
         </>
-    );
+      ) : null}
+      {Success ? (
+        <>
+          <Toast className="Success Toast" text={Success} />
+        </>
+      ) : null}
+      {Error ? (
+        <>
+          <Toast className="Error Toast" text={Error} />
+        </>
+      ) : null}
+      {DisplayLoading ? (
+        <>
+          <Loading />
+        </>
+      ) : null}
+
+      {UserData && UserHeaders ? (
+        <>
+          <Home
+            UserData={UserData}
+            UserHeaders={UserHeaders}
+            setUserData={(i) => setUserData(i)}
+            setUserHeaders={(i) => setUserHeaders(i)}
+            setDisplayLoading={(i) => setDisplayLoading(i)}
+            setMessage={(i) => setMessage(i)}
+            setSuccess={(i) => setSuccess(i)}
+            setError={(i) => setError(i)}
+          />
+        </>
+      ) : (
+        <>
+          <Login
+            setUserData={(i) => setUserData(i)}
+            setUserHeaders={(i) => setUserHeaders(i)}
+            setDisplayLoading={(i) => setDisplayLoading(i)}
+            setMessage={(i) => setMessage(i)}
+            setSuccess={(i) => setSuccess(i)}
+            setError={(i) => setError(i)}
+          />
+        </>
+      )}
+    </>
+  );
 }
